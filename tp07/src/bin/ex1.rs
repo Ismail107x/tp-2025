12fdfaef2049ddfd2b8cd99867510e8a3a3e89a3
Ismail Sghir <ismailsghir107@gmail.com>
2025-04-15 10:33:44 +0200

    Update ex1.rs

--- tp07/src/bin/ex1.rs
+++ tp07/src/bin/ex1.rs
@@ -8,17 +8,27 @@ extern "C" fn handle_signal(signal: libc::c_int) {
     println!("received signal {}: {}", signal, signal.as_str());
 
     // TODO 1: For each signal, print the specified letter
+    match signal {
+        Signal::SIGHUP => print!("a"),
+        Signal::SIGINT => print!("b"),
+        Signal::SIGQUIT => print!("n"),
+        Signal::SIGTRAP => print!("s"),
+        Signal::SIGFPE => print!(" "),
+        _ => {
+        }
+    }
 }
 
 fn main() {
     let signals = [
-        Signal::SIGHUP,
-        Signal::SIGINT,
-        Signal::SIGQUIT,
-        Signal::SIGTRAP,
-        Signal::SIGFPE,
+        Signal::SIGHUP,  // a
+        Signal::SIGINT,  // b
+        Signal::SIGQUIT, // n
+        Signal::SIGTRAP, // s
+        Signal::SIGFPE,  // space
     ];
     let handler = SigHandler::Handler(handle_signal);
+    println!("My pid is {}", std::process::id());
     for signal in signals {
         // Registering the handler for each signal
         unsafe { signal::signal(signal, handler) }.unwrap();
